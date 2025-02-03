15f73a6170128e716407d431cfc536af15e639c9
elcarp <lise.carpenter@gmail.com>
2025-02-03 14:27:06 +0700

    WIP gender selection

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -1,26 +1,37 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { Datepicker } from 'flowbite-react'
+import { Datepicker, Dropdown, Select } from 'flowbite-react'
 import Cookies from 'js-cookie'
 import moment from 'moment'
 
 export default function Calculator() {
   const bdayCookie = Cookies.get('bdayCookie')
+  const genderCookie = Cookies.get('genderCookie')
   const [birthdate, setBirthdate] = useState<Date | null>(
     bdayCookie ? new Date(bdayCookie) : new Date()
   )
+  const [gender, setGender] = useState(genderCookie ? genderCookie : 'neutral')
+
   const [age, setAge] = useState<number | null>(null)
 
   useEffect(() => {
     if (birthdate) {
       Cookies.set('bdayCookie', birthdate.toISOString(), {
-        path: '/', // Available across the entire site
-        sameSite: 'strict', // Provides some protection against CSRF
+        path: '/',
+        sameSite: 'strict',
       })
     }
   }, [birthdate])
 
+  useEffect(() => {
+    if (gender) {
+      Cookies.set('genderCookie', gender, {
+        path: '/',
+        sameSite: 'strict',
+      })
+    }
+  }, [birthdate])
   useEffect(() => {
     if (birthdate) {
       setAge(calculate_age(birthdate) || null)
@@ -37,6 +48,7 @@ export default function Calculator() {
       return null
     }
   }
+  console.log(gender, 'gender?')
   return (
     <>
       <div className='max-w-4xl grid grid-cols-2 mx-auto gap-4'>
@@ -49,11 +61,37 @@ export default function Calculator() {
             onChange={(date) => setBirthdate(date)}
           />
         </div>
-        {bdayCookie && age !== null ? (
-          <div className='flex justify-center'>Baby is {age} months old</div>
-        ) : (
-          ''
-        )}
+        <div>
+          {bdayCookie && age !== null ? (
+            <div className='flex justify-center'>Baby is {age} months old</div>
+          ) : (
+            ''
+          )}
+        </div>
+        <div>
+          <label htmlFor='gender' className='block mb-5'>
+            Baby is a
+          </label>
+          <Dropdown
+            label='Gender'
+            value={gender}
+            onChange={() => setGender(gender)}>
+            <Dropdown.Item onClick={() => setGender('male')}>Boy</Dropdown.Item>
+            <Dropdown.Item onClick={() => setGender('female')}>
+              Girl
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => setGender('neutral')}>
+              Prefer not to say
+            </Dropdown.Item>
+          </Dropdown>
+        </div>
+        <div>
+          {bdayCookie && age !== null ? (
+            <div className='flex justify-center'>Baby is a {gender} </div>
+          ) : (
+            ''
+          )}
+        </div>
       </div>
     </>
   )
